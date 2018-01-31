a8969dcdce736ee3f9272602961e8308a749b242
mustafabhatkar <mustafabhatkar@gmail.com>
2018-01-31 10:47:05 +0530

    DirectionApiContext for time and distance

--- app/src/main/java/bin/project/binmanager/MapsActivity.java
+++ app/src/main/java/bin/project/binmanager/MapsActivity.java
@@ -17,6 +17,16 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.maps.DirectionsApi;
+import com.google.maps.GeoApiContext;
+import com.google.maps.errors.ApiException;
+import com.google.maps.model.DirectionsResult;
+import com.google.maps.model.TravelMode;
+
+import org.joda.time.DateTime;
+
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
     double[][] latLon = {{19.026756, 73.055807},{19.027111, 73.057295},{19.025488, 73.054763},{19.026606, 73.055233},{19.026264, 73.056633}};
@@ -33,64 +43,69 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
+
+        DateTime now = new DateTime();
+        try {
+            DirectionsResult result = DirectionsApi.newRequest(getGeoContext())
+                    .mode(TravelMode.WALKING)
+                    .origin(new com.google.maps.model.LatLng(19.026756,73.055807))
+                    .destination(new com.google.maps.model.LatLng(19.027111,73.057295))
+                    .departureTime(now)
+                    .await();
+            Log.d(TAG, "Time :"+ result.routes[0].legs[0].duration.humanReadable + " Distance :"
+                    + result.routes[0].legs[0].distance.humanReadable);
+        } catch (ApiException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
         database = FirebaseDatabase.getInstance();
-        myRef = database.getReference("Bins").child("fill_level");
+        myRef = database.getReference("Bins");
 
     }
+
+    public GeoApiContext getGeoContext() {
+        GeoApiContext geoApiContext = new GeoApiContext.Builder()
+                .apiKey(getString(R.string.directionsApiKey))
+                .queryRateLimit(3)
+                .connectTimeout(1,TimeUnit.SECONDS)
+                .readTimeout(1, TimeUnit.SECONDS)
+                .writeTimeout(1, TimeUnit.SECONDS)
+                .build();
+        return geoApiContext;
+    }
+
 
     @Override
     public void onMapReady(final GoogleMap map) {
-
-        map.addMarker(new MarkerOptions()
-                .position(new LatLng(19.026738, 73.055215)));
-
-        for (int i = 0;i<latLon.length;i++){
-           marker =  createMarker(map, latLon[i][0], latLon[i][1], R.drawable.ic_bin_normal);
-        }
-        map.moveCamera(CameraUpdateFactory.newLatLngZoom(
-                new LatLng(19.026738, 73.055215), 18));
-
-        myRef.addValueEventListener(new ValueEventListener() {
+        myRef.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
-                // This method is called once with the initial value and again
-                // whenever data at this location is updated.
-                Long fillLevel = dataSnapshot.getValue(Long.class);
-                Log.d(TAG, "Value is: " + fillLevel);
-                marker.remove();
-                if (fillLevel<30){
-                    map.addMarker(new MarkerOptions()
-                            .position(new LatLng(19.026264, 73.056633))
-                                    .anchor(0.5f, 0.5f)
-                                    .title("Bin level")
-                                    .snippet("90%")
-                                    .icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_bin_full)));
-                }else {
-                    map.addMarker(new MarkerOptions()
-                                    .position(new LatLng(19.026264, 73.056633))
-                                    .anchor(0.5f, 0.5f)
-                                    .title("Bin level")
-                                    .snippet("40%")
-                                    .icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_bin_normal)));
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    Bins bins = snapshot.getValue(Bins.class);
+                    marker =  createMarker(map, bins.lat, bins.lng, R.drawable.ic_bin_normal,bins.fill_level);
                 }
-
             }
 
             @Override
-            public void onCancelled(DatabaseError error) {
-                // Failed to read value
-                Log.w(TAG, "Failed to read value.", error.toException());
+            public void onCancelled(DatabaseError databaseError) {
+
             }
         });
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(
+                new LatLng(19.026738, 73.055215), 18));
     }
 
-    protected Marker createMarker(GoogleMap map, double latitude, double longitude, int iconResID) {
+    protected Marker createMarker(GoogleMap map, double latitude, double longitude, int iconResID,long fill_level) {
 
         return map.addMarker(new MarkerOptions()
                 .position(new LatLng(latitude, longitude))
                 .anchor(0.5f, 0.5f)
                 .title("Bin level")
-                .snippet("40")
+                .snippet(String.valueOf(fill_level))
                 .icon(BitmapDescriptorFactory.fromResource(iconResID)));
     }
 
