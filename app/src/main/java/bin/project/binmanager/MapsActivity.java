26ff98582e2bc0e45629131ba5f65688a15a44c8
mustafabhatkar <mustafabhatkar@gmail.com>
2018-01-31 13:35:35 +0530

    distanceToDb method added

--- app/src/main/java/bin/project/binmanager/MapsActivity.java
+++ app/src/main/java/bin/project/binmanager/MapsActivity.java
@@ -2,7 +2,6 @@ package bin.project.binmanager;
 
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
-import android.util.Log;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -29,11 +28,15 @@ import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
-    double[][] latLon = {{19.026756, 73.055807},{19.027111, 73.057295},{19.025488, 73.054763},{19.026606, 73.055233},{19.026264, 73.056633}};
-    Marker marker;
-    FirebaseDatabase database;
-    DatabaseReference myRef;
-    String TAG = MapsActivity.class.getSimpleName();
+    private double[][] latLon = new double[100][2];
+    //{{19.026756, 73.055807}, {19.027111, 73.057295}, {19.025488, 73.054763}, {19.026606, 73.055233}, {19.026264, 73.056633}};
+    private Marker marker;
+    private FirebaseDatabase database;
+    private DatabaseReference myRef;
+    private String TAG = MapsActivity.class.getSimpleName();
+    private double lat, lng;
+    private long fill_level;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -44,34 +47,16 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mapFragment.getMapAsync(this);
 
 
-        DateTime now = new DateTime();
-        try {
-            DirectionsResult result = DirectionsApi.newRequest(getGeoContext())
-                    .mode(TravelMode.WALKING)
-                    .origin(new com.google.maps.model.LatLng(19.026756,73.055807))
-                    .destination(new com.google.maps.model.LatLng(19.027111,73.057295))
-                    .departureTime(now)
-                    .await();
-            Log.d(TAG, "Time :"+ result.routes[0].legs[0].duration.humanReadable + " Distance :"
-                    + result.routes[0].legs[0].distance.humanReadable);
-        } catch (ApiException e) {
-            e.printStackTrace();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
         database = FirebaseDatabase.getInstance();
         myRef = database.getReference("Bins");
 
     }
 
-    public GeoApiContext getGeoContext() {
+    private GeoApiContext getGeoContext() {
         GeoApiContext geoApiContext = new GeoApiContext.Builder()
                 .apiKey(getString(R.string.directionsApiKey))
                 .queryRateLimit(3)
-                .connectTimeout(1,TimeUnit.SECONDS)
+                .connectTimeout(1, TimeUnit.SECONDS)
                 .readTimeout(1, TimeUnit.SECONDS)
                 .writeTimeout(1, TimeUnit.SECONDS)
                 .build();
@@ -84,10 +69,21 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         myRef.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
+                int i = 0;
                 for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
                     Bins bins = snapshot.getValue(Bins.class);
-                    marker =  createMarker(map, bins.lat, bins.lng, R.drawable.ic_bin_normal,bins.fill_level);
+                    lat = bins.lat;
+                    lng = bins.lng;
+                    fill_level = bins.fill_level;
+                    marker = createMarker(map, lat, lng, R.drawable.ic_bin_normal, fill_level);
+                    latLon[i][0] = lat;
+                    latLon[i][1] = lng;
+                    i++;
                 }
+                for (int j = 0; j < dataSnapshot.getChildrenCount(); j++) {
+                    addDistanceOfBinsToDb(dataSnapshot.getChildrenCount(), latLon[j][0], latLon[j][1], j);
+                }
+
             }
 
             @Override
@@ -99,7 +95,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 new LatLng(19.026738, 73.055215), 18));
     }
 
-    protected Marker createMarker(GoogleMap map, double latitude, double longitude, int iconResID,long fill_level) {
+    protected Marker createMarker(GoogleMap map, double latitude, double longitude, int iconResID, long fill_level) {
 
         return map.addMarker(new MarkerOptions()
                 .position(new LatLng(latitude, longitude))
@@ -109,6 +105,38 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 .icon(BitmapDescriptorFactory.fromResource(iconResID)));
     }
 
+
+    private void addDistanceOfBinsToDb(long noOFbins, double lat, double lng, int currentBin) {
+        DateTime now = new DateTime();
+        try {
+
+            for (int i = 1; i <= noOFbins; i++) {
+                if (lat != latLon[i - 1][0]) {
+                    DirectionsResult result = DirectionsApi.newRequest(getGeoContext())
+                            .mode(TravelMode.DRIVING)
+                            .origin(new com.google.maps.model.LatLng(lat, lng))
+                            .destination(new com.google.maps.model.LatLng(latLon[i - 1][0], latLon[i - 1][1]))
+                            .departureTime(now)
+                            .await();
+                    int binNumber = i;
+                    myRef.child(String.valueOf(currentBin + 1))
+                            .child("distance from " + binNumber)
+                            .setValue(result.routes[0].legs[0].distance.humanReadable);
+                }
+            }
+
+//            Log.d(TAG, "Time :" + result.routes[0].legs[0].duration.humanReadable + " Distance :"
+//                    + result.routes[0].legs[0].distance.humanReadable);
+
+        } catch (ApiException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
+
+}
 
 
